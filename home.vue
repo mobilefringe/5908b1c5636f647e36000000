723d140fc609281bc10622c393e4e07101fd1a12
ikvir@mobilefringe.com @ CodeCloud <ikvir@mobilefringe.com @ CodeCloud>
2017-05-02 12:24:57 -0400

    Single Page Application with Vue.js 1.0  /home.vue

---
+++ home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row">
+    <div class="large-6 columns">
+      <h1>{{title}}</h1>
+      <p>{{description}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  define(["Vue"], function(Vue) {
+    return Vue.component("home-component", {
+      template: template, // the variable template will be injected
+      data: function() {
+        return {
+          title: "MM with Vue.js!",
+          description: "An example of integration of Mall Maverick with Vue.js"
+        }
+      }
+    })
+  })
+</script>
