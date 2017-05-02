8b67dbd7e94730fc1062077c5bc3a4f0b8618afd
ikvir@mobilefringe.com @ CodeCloud <ikvir@mobilefringe.com @ CodeCloud>
2017-05-02 12:25:38 -0400

    Single Page Application with Vue.js 1.0  /stores.vue

---
+++ stores.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
+    <div class="columns large-3 medium-6" v-for="store in stores">
+      <div class="card">
+        <div class="card-divider">
+          {{ store.name }}
+        </div>
+        <a :href="store.image_url" target="_blank"><img :src="store.image_url"></a>
+        <div class="card-section">
+          <p>{{ store.description }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  define(["Vue"], function(Vue) {
+    return Vue.component("stores-component", {
+      template: template, // the variable template will be injected
+      computed: {
+        stores () {
+          return this.$store.getters.processedStores;
+        }
+      }
+    });
+  });
+</script>
